df429ee551d5f79c26ea979ce7e94ef955025b18
chicco4 <chiccotto4@gmail.com>
2022-05-10 17:35:49 +0200

    03-04-05-06-07-08 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
 
+//[°C] = ([°F] − 32) × 5⁄9
+const ftoc = function (n) {
+
+  return Math.round(((n - 32) * (5 / 9)) * 10) / 10;
 };
 
-const ctof = function() {
+//[°F] = [°C] × 9⁄5 + 32
+const ctof = function (n) {
 
+  return Math.round(((n * (9 / 5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+  return (n1 - n2);
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let mul = 1;
+  arr.forEach(element => {
+    mul *= element;
+  });
+  return mul;
 };
 
-const power = function() {
-	
+const power = function (n1, n2) {
+  let ans = 1;
+  for (let i = 0; i < n2; i++) {
+    ans*=n1;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 0) 
+        return -1;
+  else if (n == 0) 
+      return 1;
+  else {
+      return (n * factorial(n - 1));
+  }
 };
 
 // Do not edit below this line
