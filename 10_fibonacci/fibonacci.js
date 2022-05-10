c1685d462af9b41fa6e100b21ccbc3c85397e25e
chicco4 <chiccotto4@gmail.com>
2022-05-10 22:21:36 +0200

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if(number < 0){
+        return "OOPS";
+    }
+    // program to generate fibonacci series up to n terms
+    
+    // take input from the user
+    let n1 = 0, n2 = 1, nextTerm;
 
+    for (let i = 1; i <= number; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return n1;
 };
 
 // Do not edit below this line
